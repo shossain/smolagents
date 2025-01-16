271cb6c24984772c8fc7f060e9bf8947eea335bb
Wauplin <lucainp@gmail.com>
2025-01-16 17:44:31 +0100

    Clean makefile / pyproject.toml / .github

--- examples/gradio_upload.py
+++ examples/gradio_upload.py
@@ -1,11 +1,5 @@
-from smolagents import (
-    CodeAgent,
-    HfApiModel,
-    GradioUI
-)
+from smolagents import CodeAgent, HfApiModel, GradioUI
 
-agent = CodeAgent(
-    tools=[], model=HfApiModel(), max_steps=4, verbosity_level=0
-)
+agent = CodeAgent(tools=[], model=HfApiModel(), max_steps=4, verbosity_level=0)
 
-GradioUI(agent, file_upload_folder='./data').launch()
+GradioUI(agent, file_upload_folder="./data").launch()
